8f4ec89811d7d7ff464db99cf9b2bf3547f5ff29
hequanhui <774860693@qq.com>
2024-04-13 00:55:09 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # algorithms_test
+## AStar
+- 概念
+    - 开放列表：openSet
+    - 关闭列表：closeSet
+    - 启发函数：heuristic，一般视作h
+        - 曼哈顿距离：D*(dx + dy)
+        - 欧几里得距离：D * sqrt(dx * dx + dy * dy)
+    - 起点到节点的代价：g
+    - 节点到终点的代价：f = g + h
--- src/o1.cpp
+++ src/o1.cpp
@@ -1,6 +1,81 @@
+#include <queue>
+#include <unordered_set>
+#include <vector>
+#include <unordered_map>
 
+const int width = 10;
+const int height = 10;
+
+
+struct Vertex
+{
+	int x;
+	int y;
+	bool block = false;
+	Vertex(int x1, int y1) : x(x1), y(y1) {}
+};
+std::unordered_map<std::pair<int, int>, Vertex*> vertexMap;
+
+Vertex* empty = new Vertex(-1, -1);
+
+Vertex* getVertex(int x, int y) {
+	auto key = std::pair<int, int>{ x, y };
+	auto it = vertexMap.find(key);
+	if (it == vertexMap.end()) {
+		return empty;
+	}
+	return vertexMap[{x, y}];
+}
+
+void neighbors(Vertex* vert, std::vector<Vertex*>& neigh) {
+	auto x = vert->x;
+	auto y = vert->y;
+	if (x > 0) {
+		neigh.push_back(getVertex(x - 1, y));
+	}
+
+	if (y > 0) {
+		neigh.push_back(getVertex(x, y - 1));
+	}
+
+	if (x < width -1) {
+		neigh.push_back(getVertex(x + 1, y));
+	}
+
+	if (y < height - 1) {
+		neigh.push_back(getVertex(x, y + 1));
+	}
+}
+
+
+void bfs() {
+	auto start = new Vertex(1, 1);
+	vertexMap[{1, 1}] = start;
+
+	auto frontier = std::queue<Vertex*>();
+	frontier.push(start);
+
+	auto visited = std::unordered_set<Vertex*>();
+	visited.insert(start);
+
+	while (!frontier.empty())
+	{
+		auto vertex = frontier.front();
+		frontier.pop();
+		auto neighb = std::vector<Vertex*>();
+		neighbors(vertex, neighb);
+		for (auto tmp : neighb) {
+			if (visited.count(tmp)) {
+				continue;
+			}
+			frontier.push(tmp);
+			visited.insert(tmp);
+		}
+	}
+}
 
 
 int main() {
+	bfs();
 	return 0;
 }
